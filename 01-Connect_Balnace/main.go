ee860213e78bdf238605c843c650e947556a54ad
Tanay <ankulwartanay@gmail.com>
2025-10-19 16:04:43 +0530

    Wallet Generation

--- 01-Connect_Balnace/main.go
+++ 01-Connect_Balnace/main.go
@@ -4,14 +4,24 @@ import (
 	"context"
 	"fmt"
 	"log"
-	//"math/big"
+	"math"
+	"math/big"
+
+	
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var infuraURL = "https://mainnet.infura.io/v3/a54d875e40a344ef9da422b50ca4d226"
-var address = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97"
+var address = common.HexToAddress("0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97")
+
+func etherConversion(balance *big.Int) *big.Float {
+	fBalance := new(big.Float)
+	fBalance.SetInt(balance)
+	EtherValue := new(big.Float).Quo(fBalance, big.NewFloat(math.Pow10(18)))
+	return EtherValue
+}
 
 func main() {
 	client, err := ethclient.DialContext(context.Background(), infuraURL)
@@ -27,9 +37,9 @@ func main() {
 
 	fmt.Println("Latest block number:", block.Number())
 
-	balance, err := client.BalanceAt(context.Background(), common.HexToAddress(address) , nil)
+	balance, err := client.BalanceAt(context.Background(), address , nil)
 	if err != nil {
 		log.Fatalf("Error getting balance: %v", err)
 	}
-	fmt.Println("Balance: ", balance)
+	fmt.Printf("Balance: %.4f ETH ", etherConversion(balance))
 }
